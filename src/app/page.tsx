2908c2c2e04476ba8037fc58d35b9a36531324db
Nonthpat Wongkham <bnbn5588@hotmail.com>
2024-08-25 08:16:00 +0700

    install speedinsights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HomePage from './home';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
 
       <HomePage />
       <Analytics />
+      <SpeedInsights />
       <Footer />
     </>
   );
